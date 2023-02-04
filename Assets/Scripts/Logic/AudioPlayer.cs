45247d6029f5b07f55dcaa6e93029108177b3346
Frederik Krumm <frederik.krumm@smail.inf.h-brs.de>
2023-02-04 18:35:34 +0100

    Added more content and hidden note

---
+++ Assets/Scripts/Logic/AudioPlayer.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+public class AudioPlayer : MonoBehaviour
+{
+    public float volume;
+    public void PlayAudio()
+    {
+
+    }
+
+    public void StopAudio()
+    {
+
+    }
+}
